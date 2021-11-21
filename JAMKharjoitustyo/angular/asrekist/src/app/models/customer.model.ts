cf70e03d1e6bd4b31e067ec0f6bb3c966eae92ae
Tanja Koivunen <tanja.koivunen@elisanet.fi>
2021-11-21 19:06:13 +0200

    Front_2.versio_1

--- JAMKharjoitustyo/angular/asrekist/src/app/models/customer.model.ts
+++
@@ -1,8 +0,0 @@
-export class Customer {
-  id?: any;
-  email?: string;
-  name?: string;
-  address?: string;
-  info?: string;
-  active?: string;
-}
--- JAMKharjoitustyo/angular/asrekisteri/src/app/models/customer-aloitussivu.model.ts
+++
@@ -1,8 +0,0 @@
-export class CustomerModel {
-  id: number = 0;
-  name: string = '';
-  email: string = '';
-  address: string = '';
-  info: string = '';
-  active: string = '';
-}
